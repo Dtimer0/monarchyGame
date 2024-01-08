61987bb89a69f6924d012005c2d6b4b7f0429f39
Jude Packard-Jones <judepackardjones@gmail.com>
2024-01-08 11:40:54 -0500

    ' main, + quiz variants

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,25 @@ mod variants {
 pub mod toolbox;
 pub mod types;
 
-use variants::quiz as quiz;
-use variants::cyoa as cyoa;
+use variants::cyoa;
+use variants::quiz;
 
 use crate::toolbox::readline;
-use crate::variants::quiz::quiz;
 use crate::variants::cyoa::cyoa;
-fn main() { 
+use crate::variants::quiz::quiz;
+
+fn main() {
     println!("Hello, welcome to this text-based adventure. In this game, you will go through a series of questions, which you will answer by entering text into the console. You have two options: Play this game as a quiz(1), or play this game as a choose your own adventure(2). Which do you choose? Press 1 go play the game as a quiz, or 2 to play it as a choose your own adventure.");
-    if readline() == "1" {
-        quiz();
-    } else {
-        println!("Under Construction");
+    loop {
+        let option = readline();
+        if option == "1" {
+            quiz();
+        } else if option == "2" {
+            cyoa();
+        } else {
+            println!("Please enter a valid option(1 or 2).");
+            continue;
+        }
+        break;
     }
 }
--- src/variants/quiz.rs
+++ src/variants/quiz.rs
@@ -1,3 +1,31 @@
+use crate::toolbox::readline;
+use crate::types::*;
+
 pub fn quiz() {
-    println!("Hello, world!");
+    println!("Welcome to the quiz variant of this game! Here, you will be asked a series of questions from the perspective 
+    of the current prime minister of Canada at the time of the question. You will be given four options, and you must choose which one is correct. You have
+    2 playable options, play this game as a test, where your score will be tallied at the end, and you will be given a grade, or,
+    play it as a learning walkthrough, where after each question, you will be given the correct answer, and an explanation of why it is correct.
+    In the test, entering any text that does not point to an answer into the console will be considered as leaving the question blank, and will be,
+    marked as incorrect. In the walkthrough, you will be able to enter any text into the console, and it will be considered as entering the correct answer.
+    Enter 1 to play the test, or 2 to play the walkthrough.");
+    loop {
+        let option = readline();
+        if option == "1" {
+            quiz_test();
+        } else if option == "2" {
+            quiz_walkthrough();
+        } else {
+            println!("Please enter a valid option(1 or 2).");
+            continue;
+        }
+        break;
+    }
+}
+fn quiz_test() {
+    let quiz: [QuizBlock; 17] = [];
+    let mut score: i8 = 0;
+}
+fn quiz_walkthrough() {
+
 }
